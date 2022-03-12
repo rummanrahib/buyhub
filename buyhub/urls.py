c9d93107a93d493d90d3aa0d0b37fc1132de69ea
Rumman Rahib <rummanrahib@outlook.com>
2022-03-13 00:19:02 +0600

    v1 before deployment

---
+++ buyhub/urls.py
@@ -0,0 +1,18 @@
+from xml.etree.ElementInclude import include
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
+
+    path('orders/', include('orders.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
